61fc0ea9ecd3664ec2c96f6df51324d9dc053309
Kovács Levente <le.kovacs789@gmail.com>
2026-04-28 00:17:20 +0300

    moved images from public to src

--- src/components/UserLogin/LoginView.vue
+++ src/components/UserLogin/LoginView.vue
@@ -52,7 +52,7 @@ async function validateLoginInput() {
     <div class="relative z-20 m-4 flex flex-col items-center justify-center gap-y-3 md:pt-10">
       <div class="flex flex-col items-center gap-y-5">
         <div class="">
-          <img src="/public/logo_with_title.png" alt="Ludario logo" />
+          <img src="/src/assets/images/logo_with_title.png" alt="Ludario logo" />
         </div>
         <span class="py-3 text-center text-lg font-bold md:text-2xl"
           >An online board game shop for newcomers and veterans alike</span
@@ -101,8 +101,8 @@ async function validateLoginInput() {
       </div>
     </div>
     <div class="fixed bottom-0 z-10 flex w-dvw flex-row">
-      <img src="/public/images/bottom_hexagons_updated.png" class="md:hidden" />
-      <img src="/public/images/bottom_hexagons_full.png" class="hidden md:inline" />
+      <img src="/src/assets/images/bottom_hexagons_updated.png" class="md:hidden" />
+      <img src="/src/assets/images/bottom_hexagons_full.png" class="hidden md:inline" />
     </div>
     <div
       class="fixed inset-0 z-50 border-t-8 border-t-orange-600 bg-slate-500 transition-transform duration-700 ease-in-out"
