54640271bca3e28e61672908449e4815c6c868c8
Kovács Levente <le.kovacs789@gmail.com>
2026-05-01 20:26:43 +0300

    set up for access outside of localhost

--- vite.config.ts
+++ vite.config.ts
@@ -8,15 +8,25 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    tailwindcss()
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    host: '0.0.0.0', // listen on all interfaces, not just localhost
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://192.168.100.1:8000', // main system's IP
+        changeOrigin: true,
+      },
+      '/ws': {
+        target: 'ws://192.168.100.1:8000', // WebSocket proxy
+        ws: true,
+        changeOrigin: true,
+      },
     },
   },
 })
