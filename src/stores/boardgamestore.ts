9b0ec6a4da66df4a85b6d5cec1f77f2f24caea94
Kovács Levente <le.kovacs789@gmail.com>
2026-05-01 18:21:20 +0300

    remove board game store

--- src/stores/boardgamestore.ts
+++
@@ -1,6 +0,0 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
-import { createBoardGame, type BoardGame } from '@/data/boardgame.ts'
-import { boardgameApi } from '@/api_services/api'
-
-export const useBoardGamesStore = defineStore('boardgames', () => {})
