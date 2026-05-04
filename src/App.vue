4154cc9b49c70eb9c32ddc8c154bb5d479a62468
Kovács Levente <le.kovacs789@gmail.com>
2026-05-05 01:57:40 +0300

    removed reference to user store

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,5 @@
 <script setup lang="ts">
-import { useUserStore } from './stores/userstore'
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
-
-const userstore = useUserStore()
-userstore.resetUsers()
-
-userstore.addNewUser({
-  email: 'le.kovacs789@gmail.com',
-  username: 'Leti',
-  passwordhash: '5c1a9af982e629455d7e0f830b2c387eb02a12a953f59b560893f1152fe7fd57',
-  isAdmin: true,
-})
 </script>
 
 <template>
