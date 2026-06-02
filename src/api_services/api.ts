b2a5690d133fae95669e00ac3efed0a760080b84
Kovács Levente <le.kovacs789@gmail.com>
2026-06-02 16:10:48 +0300

    another bugfix

--- src/api_services/api.ts
+++ src/api_services/api.ts
@@ -7,7 +7,7 @@ import { useUserStore } from '@/stores/userstore'
 // maybe in the future refactor, so that api isn't exposed
 // temporary botch for api_connection_check.ts
 export const api = axios.create({
-  baseURL: 'https://ludario-project-backend.onrender.com:8000',
+  baseURL: 'https://ludario-project-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
